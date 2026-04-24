117cc348bee12db0bedadeee4babc4e06887461a
Eric Chen <fshiori@gmail.com>
2026-04-24 15:24:52 +0800

    feat: rebrand to Magi Lab with extreme dark mode and custom projects

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,25 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+		themes: [
+			{
+				magilab: {
+					"primary": "#ffffff",
+					"secondary": "#f3f4f6",
+					"accent": "#37cdbe",
+					"neutral": "#3d4451",
+					"base-100": "#050505",
+					"base-200": "#0a0a0a",
+					"base-300": "#141414",
+					"info": "#2094f0",
+					"success": "#009485",
+					"warning": "#ff9900",
+					"error": "#ff5724",
+				},
+			},
+			"dark",
+		],
+		darkTheme: "magilab",
+		logs: false,
+	}
 }
